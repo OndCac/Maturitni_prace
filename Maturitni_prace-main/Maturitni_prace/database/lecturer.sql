5b04977f958e7e562812b04eaefd84096c06d02b
Ondřej Cacek <144690836+OndCac@users.noreply.github.com>
2024-01-08 13:24:03 +0100

    Add files via upload

---
+++ Maturitni_prace-main/Maturitni_prace/database/lecturer.sql
@@ -0,0 +1,62 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- Create the database
+CREATE DATABASE IF NOT EXISTS TeacherDigitalAgency;
+USE TeacherDigitalAgency;
+
+-- Create the Contact table
+CREATE TABLE IF NOT EXISTS Contact (
+    UUID INT(36) auto_increment PRIMARY KEY,
+    LecturerUUID INT,
+    TelephoneNumbers JSON,
+    Emails JSON,
+	FOREIGN KEY (LecturerUUID) REFERENCES Lecturer(UUID)
+);
+
+-- Create the Tag table
+CREATE TABLE IF NOT EXISTS Tag (
+    UUID INT(36) auto_increment PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+-- Create the Lecturer table
+CREATE TABLE IF NOT EXISTS Lecturer (
+    UUID INT(36) auto_increment PRIMARY KEY,
+    TitleBefore VARCHAR(10),
+    FirstName VARCHAR(50),
+    MiddleName VARCHAR(50),
+    LastName VARCHAR(50),
+    TitleAfter VARCHAR(10),
+    PictureURL VARCHAR(255),
+    Location VARCHAR(50),
+    Claim TEXT,
+    Bio TEXT,
+    PricePerHour DECIMAL(10, 2),
+    PRIMARY_CONTACT_UUID INT,
+    FOREIGN KEY (PRIMARY_CONTACT_UUID) REFERENCES Contact(UUID)
+);
+
+-- Create the LecturerTag table
+CREATE TABLE if not exists LecturerTag (
+    LecturerUUID INT(36),
+    TagUUID INT(36),
+    PRIMARY KEY (LecturerUUID, TagUUID),
+    FOREIGN KEY (LecturerUUID) REFERENCES Lecturer(UUID),
+    FOREIGN KEY (TagUUID) REFERENCES Tag(UUID)
+);
+
+-- Create the User table
+
+CREATE TABLE if not exists User (
+    UUID int not null auto_increment primary KEY ,
+    UserName VARCHAR(50) not null,
+    Email VARCHAR(50) not null unique,
+    Password VARCHAR(50) not null
+);
+
+alter table User
+drop column LoggedIn;
---
+++ Maturitni_prace-main/Maturitni_prace/public/index.php
@@ -0,0 +1,52 @@
+<?php
+    // def. odradkovani v HTML
+    define("BR", "<br/>\n");
+
+    session_start();
+    if (empty($_SESSION["logged_in"])) {
+        $_SESSION["logged_in"] = false;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" type="text/css"/>
+    <title>TdA</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li class="aktivni"><a href="index.php">Homepage</a></li>
+                <?php 
+                    if ($_SESSION["logged_in"]) {
+                        echo "<li><a href='lec_list.php'>Lecturers</a></li>";
+                    } else {
+                        echo '<li><a href="registration.php">Registration</a></li>' 
+                        . '<li><a href="login.php">Login</a></li>';
+                    }
+                ?>
+            </ul>
+        </nav>
+    </header>
+    
+    <article>
+        <header>
+            <h1>Homepage</h1>
+        </header>
+        <section>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quisquam repellendus! Nihil delectus quasi cumque odit vel totam labore officiis enim ullam optio? Exercitationem quae distinctio mollitia nemo nobis ad!
+            </p>
+        </section>
+    </article>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
